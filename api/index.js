996cd115f61ec01baf5d405e511ca12ff5601039
SachinKathad <136622717+SachinKathad@users.noreply.github.com>
2024-11-21 10:57:28 +0530

    database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-import { log } from 'console'
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGOOSE).then(()=>{
+  console.log("connected to mongodb");
+  
+})
+.catch((err)=>{
+  console.log(err);
+  
+})
 
 const app = express()
 
